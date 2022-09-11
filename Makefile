c9df90257a485072fa91d35edb08c1fe41556a71
Jefferson472 <jefferson472@hotmail.com>
2022-09-11 13:09:34 -0300

    feat: add list and detail view

--- Makefile
+++ Makefile
@@ -1,3 +1,22 @@
 migrations:
 	python3 src/manage.py makemigrations
 	python3 src/manage.py migrate
+
+pip:
+	pip install -r requirements.txt
+	pip freeze > requirements.txt
+
+runserver:
+	python3 src/manage.py runserver
+
+shell:
+	python3 src/manage.py shell
+
+venv:
+	source venv/bin/activate
+
+venv-d:
+	source venv/Scripts/deactivate.bat
+
+venv-venv:
+	virtualenv venv
--- src/apps/ecommerce/views.py
+++ src/apps/ecommerce/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from unicodedata import category
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Category, Product
+
+
+class ProductListView(ListView):
+    model = Product
+    queryset = Product.objects.filter(available=True)
+
+    def get_queryset(self, **kwargs):
+        queryset = super().get_queryset()
+        if kwargs.get('category_slug'):
+            queryset = queryset.filter(category=kwargs['category_slug'])
+        return queryset
+
+
+class ProductDetailView(DetailView):
+    model = Product
