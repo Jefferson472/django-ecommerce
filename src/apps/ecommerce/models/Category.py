be1ce2e2769c319ee3a0a52c9bbf77a586755787
Jefferson472 <jefferson472@hotmail.com>
2022-09-11 09:14:36 -0300

    feat: add models Product and Category

---
+++ src/apps/ecommerce/models/Category.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)    
+
+    class Meta:
+        db_table = 'tb_category'
+        ordering = ('name',)
+        verbose_name = ("Categoria")
+        verbose_name_plural = ("Categorias")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("_detail", kwargs={"pk": self.pk})
