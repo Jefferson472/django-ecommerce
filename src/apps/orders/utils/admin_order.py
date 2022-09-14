d9c184fe2c586d7a6791b1442cdf6a80d1cd6cb4
Jefferson472 <jefferson472@hotmail.com>
2022-09-14 11:34:53 -0300

    fix: static files path and utils admin order

--- src/apps/orders/utils/admin_order.py
+++ src/apps/orders/utils/admin_order.py
@@ -5,3 +5,8 @@ from django.utils.safestring import mark_safe
 def order_detail(obj):
     url = reverse('orders:admin_order_detail', args=[obj.id])
     return mark_safe(f'<a href="{url}">View</a>')
+
+
+def order_pdf(obj):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    return mark_safe(f'<a href="{url}">PDF</a>')
