a15296afc8d27b6be2dbd8ace60e58bd922e6b20
Jefferson472 <jefferson472@hotmail.com>
2022-09-14 12:52:59 -0300

    feat: add coupon system in Cart Class

--- src/apps/cart/cart.py
+++ src/apps/cart/cart.py
@@ -1,8 +1,8 @@
 from decimal import Decimal
 from django.conf import settings
 
-from ..ecommerce.models import Product
-
+from apps.ecommerce.models import Product
+from apps.coupons.models import Coupon
 
 class Cart(object):
     def __init__(self, request) -> None:
@@ -13,6 +13,7 @@ class Cart(object):
             # salva um carrinho vazio na sessão
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+        self.coupon_id = self.session.get('coupon_id')
 
     def __iter__(self):
         """ Itera pelos itens do carrinho de compras e obtém os produtos do banco de dados """
@@ -66,3 +67,22 @@ class Cart(object):
         # remove o carrinho da sessão
         del self.session[settings.CART_SESSION_ID]
         self.save()
+
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            try:
+                return Coupon.objects.get(id=self.coupon_id)
+            except Coupon.DoesNotExist:
+                pass
+        return None
+
+    def get_discount(self):
+        if self.coupon:
+            return (
+                self.coupon.discount / Decimal(100) * self.get_total_price()
+            )
+        return Decimal(0)
+
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount
