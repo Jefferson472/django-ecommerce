2224ed0bc9a49aa9d167cbb3d40c4efc2983b8c0
Jefferson472 <jefferson472@hotmail.com>
2022-09-15 08:08:27 -0300

    feat: add urls locale principal

--- src/setup/urls.py
+++ src/setup/urls.py
@@ -1,10 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path, include
 
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path("admin/", admin.site.urls),
     path('cart/', include('apps.cart.urls')),
     path('orders/', include('apps.orders.urls')),
@@ -12,7 +13,7 @@ urlpatterns = [
     path('coupons/', include('apps.coupons.urls')),
     path('rosetta/', include('rosetta.urls')),
     path('', include('apps.ecommerce.urls')),
-]
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
