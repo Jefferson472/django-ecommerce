c29734da0fb2f37f0fd5fec620ca0a19191e76e3
Jefferson Miranda <jefferson472@hotmail.com>
2023-08-06 16:50:14 -0300

    feat: add url ecommerce locale

--- src/apps/ecommerce/urls.py
+++ src/apps/ecommerce/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.conf.urls.i18n import i18n_patterns
+from django.utils.translation import gettext_lazy as _
 
 from .views import ProductListView, ProductDetailView
 
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
-    path('product-category/<slug:slug>', ProductListView.as_view(), name='product_list_by_category'),
-    path('product/<int:pk>/<slug:slug>', ProductDetailView.as_view(), name='product_detail'),
+    path(_('product-category/<slug:slug>'), ProductListView.as_view(), name='product_list_by_category'),
+    path(_('product/<int:pk>/<slug:slug>'), ProductDetailView.as_view(), name='product_detail'),
 ]
